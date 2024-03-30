a5b6e7778882bc76b9392951a97195c620d64321
* <taysongt@github.com>
2024-03-30 20:43:22 +0200

    Fixed DB Configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,8 @@ import {Blog} from './blogs/entities/blog.entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService)=>({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        url: configService.get<string>('DB_URL'),
+        ssl: true,
         entities: [join(process.cwd(), 'dist/**/*.entity{.js, .ts}')],
         
         synchronize: true,
